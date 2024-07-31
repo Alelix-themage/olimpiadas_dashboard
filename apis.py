549f6fa6fab581176dbf42d071d7b170d2176633
Alelix-themage <alessandrojunior864@gmail.com>
2024-07-31 20:28:05 -0300

    Atualizando com rodapé

--- apis.py
+++
@@ -1,5 +0,0 @@
-
-#site API = https://docs.apis.codante.io/olympic-games
-url = 'https://apis.codante.io/olympic-games/events'
-url_ranking = 'https://apis.codante.io/olympic-games/countries'
-url_jogos = 'https://apis.codante.io/olympic-games/disciplines'
